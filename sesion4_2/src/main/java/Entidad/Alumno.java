3eb4bfadb28916a554499be2766b2678bf961a34
alumno <alumno@2A5PC06.fpmislata.local>
2016-10-14 10:39:13 +0200

    Realización del ejercicio sesion4_2

---
+++ sesion4_2/src/main/java/Entidad/Alumno.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+
+public class Alumno {
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+    
+    
+}
