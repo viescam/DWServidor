ecca891f6c5f7efd5c55e20a771b6d0ee41f7c0e
alumno <alumno@2A5PC06.fpmislata.local>
2016-10-17 09:33:14 +0200

    Creación ejercicio13DWS

---
+++ ejercicio13DWS/src/main/java/alumnos/AlumnoBean.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alumnos;
+
+/**
+ *
+ * @author alumno
+ */
+public class AlumnoBean {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String dni;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido1
+     */
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    /**
+     * @param apellido1 the apellido1 to set
+     */
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    /**
+     * @return the apellido2
+     */
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    /**
+     * @param apellido2 the apellido2 to set
+     */
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+    
+}
---
+++ ejercicio13DWS/src/main/webapp/introduceDNI.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : introduceDNI
+    Created on : 17-oct-2016, 8:57:37
+    Author     : alumno
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Introduce DNI a buscar</h1>
+        <form name="introducirDNI" action="ServletAlumos" method="POST">
+            <input type="text" name="dni" value="" />
+            <input type="submit" value="Enviar" />
+        </form>
+    </body>
+</html>
