ad1bd13792f326d6563782ff4892ad64b6c731cf
alumno <alumno@2A5PC06.fpmislata.local>
2016-11-04 11:40:26 +0100

    Subiendo CalculadoraEJB-ejb

---
+++ CalculadoraEJB/CalculadoraEJB-ejb/src/main/java/ejb/CalculadoraEJBLocal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author alumno
+ */
+@Local
+public interface CalculadoraEJBLocal {
+    
+    double calculaAreaCirculo (double radio);
+    double calculaAreaCuadrado (double lado);
+    double calculaAreaTriangulo (double base,double altura);
+    double calculaAreaTrapecio (double base1,double base2,double altura);
+    
+}
